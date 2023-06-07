beb71f94b3b073f61392b119fb53b8c21df96640
Stowy <stowy@protonmail.ch>
2023-06-07 23:17:30 +0200

    Added base scene for cubemap

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <spdlog/spdlog.h>
 
 #include "window.hpp"
-#include "scenes/transparent_scene.hpp"
+#include "scenes/cubemap_scene.hpp"
 
 int main(int, char*[])
 {
@@ -9,7 +9,7 @@ int main(int, char*[])
 	spdlog::set_level(spdlog::level::debug);
 #endif
 
-	stw::Window<stw::TransparentScene> window("OpenGL Scene");
+	stw::Window<stw::CubeMapScene> window("OpenGL Scene");
 	window.Loop();
 	return 0;
 }
