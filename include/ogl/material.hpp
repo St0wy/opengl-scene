62e826e7c3cb6435b1539bc458dd40230aba9372
Stowy <stowy@protonmail.ch>
2023-06-23 17:25:11 +0200

    Added more material

--- include/ogl/material.hpp
+++ include/ogl/material.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <ogl/pipeline.hpp>
+#include <variant>
 
 namespace stw
 {
@@ -11,8 +12,18 @@ struct MaterialBase
 
 struct MaterialNormalNoSpecular
 {
-	float specular;
-	float shininess;
-
+	f32 specular;
+	f32 shininess;
+	std::size_t diffuseMapIndex;
+	std::size_t normalMapIndex;
 };
+
+struct MaterialNoNormalNoSpecular
+{
+	f32 specular;
+	f32 shininess;
+	std::size_t diffuseMapIndex;
+};
+
+using Material = std::variant<MaterialNoNormalNoSpecular, MaterialNormalNoSpecular>;
 }
